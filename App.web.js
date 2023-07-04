ed08fdf60858925caa2daec2d8c844cd025ffcb2
dolphin <pcx474@gmail.com>
2023-07-04 22:49:07 +0900

    RN web 적용

---
+++ App.web.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Switch} from 'react-native';
+
+function App() {
+  return (
+    <>
+      <h1> 드디어 완성 </h1>
+      <Switch />
+    </>
+  );
+}
+
+export default App;
